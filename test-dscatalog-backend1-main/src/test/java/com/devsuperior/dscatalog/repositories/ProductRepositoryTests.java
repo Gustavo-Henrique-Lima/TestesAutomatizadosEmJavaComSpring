32a8345c0518012c1f3d2f4bac73290f7dae9e1a
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-13 15:49:15 -0300

    feature: Added beforeEach to initialize the id variables that will be used before each test.

--- test-dscatalog-backend1-main/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
+++ test-dscatalog-backend1-main/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -15,20 +16,27 @@ public class ProductRepositoryTests {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;	
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+	}
 
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		Long existingId = 1L;
 		repository.deleteById(existingId);
 		Optional<Product> result = repository.findById(existingId);
 		Assertions.assertFalse(result.isPresent());
 	}
-	
+
 	@Test
 	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
-		Long noExistingId = 1000L; 
-		Assertions.assertThrows(EmptyResultDataAccessException.class, ()->{
-			repository.deleteById(noExistingId);
+		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
 		});
 	}
 }
