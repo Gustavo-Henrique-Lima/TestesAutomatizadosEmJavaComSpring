f54930ed1c001d82b09f22123e2f5cc977306d50
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-14 11:17:29 -0300

    test: Testing for the service layer with delete by id method with two scenarios, when the id exists and when the id does not exist.  Also configured mockito.

--- test-dscatalog-backend1-main/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ test-dscatalog-backend1-main/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,19 +1,51 @@
 package com.devsuperior.dscatalog.services;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
 
 	@InjectMocks
 	private ProductService service;
-	
+
 	@Mock
 	private ProductRepository repository;
-	
+
+	private Long existingId;
+	private Long nonExistingId;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 3000L;
+		Mockito.doNothing().when(repository).deleteById(existingId);
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
+	}
+
+	@Test
+	public void deleteShouldDoNotWhenIdExisting() {
+		Assertions.assertDoesNotThrow(() -> {
+			service.delete(existingId);
+		});
+		Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+	}
+
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesExisting() {
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+		Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
+	}
 }
