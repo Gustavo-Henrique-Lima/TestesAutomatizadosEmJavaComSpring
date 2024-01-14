a8d826664d55546513c09db633414f61d8b0d7bf
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-14 17:12:01 -0300

    test: Created tests for the web layer. Testing getAllProducts.

---
+++ test-dscatalog-backend1-main/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTests.java
@@ -0,0 +1,44 @@
+package com.devsuperior.dscatalog.resources;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.tests.ProductFactory;
+
+@WebMvcTest(ProductResource.class)
+public class ProductResourceTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	private PageImpl<ProductDTO> page;
+	private ProductDTO entityDto;
+
+	@MockBean
+	private ProductService service;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		entityDto = ProductFactory.createProductDTO();
+		page = new PageImpl<>(List.of(entityDto));
+		Mockito.when(service.findAllPaged(ArgumentMatchers.any())).thenReturn(page);
+	}
+
+	@Test
+	public void findAllShouldReturnPage() throws Exception {
+		mockMvc.perform(get("/products")).andExpect(status().isOk());
+	}
+}
